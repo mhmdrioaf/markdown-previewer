d40a2e444f5deff33fefd82a47d8e7c2f67eeebf
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-03-10 20:19:03 +0700

    chore: adding custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(var(--primary) / <alpha-value>)",
+        "primary-foreground": "rgb(var(--primary-foreground) / <alpha-value>)",
+      },
+    },
   },
   plugins: [],
 };
