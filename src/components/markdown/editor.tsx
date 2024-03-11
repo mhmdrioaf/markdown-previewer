1ee41c0e8e198a62c50da07aec35505965cf0908
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-03-11 07:40:31 +0700

    chore: implementing markdown context

--- src/components/markdown/editor.tsx
+++ src/components/markdown/editor.tsx
@@ -1,4 +1,10 @@
+import React from "react";
+import { MarkdownContext } from "../../lib/markdownContext";
+
 export default function MarkdownEditor() {
+  const { handler, markdown } = React.useContext(
+    MarkdownContext
+  ) as TMarkdownContext;
   return (
     <section className="w-full flex flex-col bg-white rounded-md overflow-hidden shadow-sm">
       <div className="w-full px-4 py-2 bg-primary text-primary-foreground font-medium text-center">
@@ -6,6 +12,9 @@ export default function MarkdownEditor() {
       </div>
 
       <textarea
+        id="editor"
+        onChange={(e) => handler.setMarkdown(e.target.value)}
+        value={markdown}
         className="w-full px-4 py-2 min-h-48 overflow-y-auto focus:outline-none border border-primary/15 rounded-b-md focus:border-primary transition-colors"
         placeholder="Write your markdown here"
       />
