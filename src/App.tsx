3701d5210fff4ddd023e93a1d0afbb8a1012faa2
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-03-11 07:41:01 +0700

    chore: adding markdown previewer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import MarkdownEditor from "./components/markdown/editor";
+import MarkdownPreviewer from "./components/markdown/previewer";
 
 export default function App() {
   return (
     <main className="w-full min-h-screen flex flex-col gap-4 items-center justify-center px-32 py-4 bg-[#FAFAFA]">
       <MarkdownEditor />
+
+      <MarkdownPreviewer />
     </main>
   );
 }
